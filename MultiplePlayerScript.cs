b14d5aec2f861f22cabd045e6084b5969111db1b
ShayJStewart <silverwingshay@gmail.com>
2016-04-26 09:25:25 -0600

    Added files via upload

---
+++ MultiplePlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiplePlayerScript : MonoBehaviour {
+	
+	private static bool playerExists;
+
+	// code for destroying duplicates of player that are created when going between levels
+	void Start () 
+	{
+		//getting animator for Characters animation and the rigidbody located on the player
+		animation = GetComponent<Animator> ();
+		MyRigidbody = GetComponent<Rigidbody> ();
+
+		//if the player exists and has not already gone to this level, then playerExists is true
+		if (!playerExists) {
+			playerExists = true;
+			DontDestoryOnLoad (transform.gameObject);
+		} 
+		//if there is already a player in the level that already fulfills the above if statement, send the second or third character to the else statement to be destoryed
+
+		else
+		{
+			Destory (gameObject);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TreasureChest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreasureChest : MonoBehaviour {
+	/*
+	public enum Treasure
+	{
+		Open,
+		Close
+	}
+	public Treasure chest;
+	public Sprite [] sprites;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TreasureChest ();
+	}
+
+	public void TreasureChest(){
+		switch (chest)
+		{
+		case Treasure.Close:
+			GetComponent <SpriteRenderer> ().sprite = sprites [0];
+			break;
+		case Treasure.Open:
+			GetComponent <SpriteRenderer> ().sprite = sprites [1];
+			break;
+		default:
+			break;
+
+		}
+
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag =="Player")
+			
+	}
+	*/
+}
