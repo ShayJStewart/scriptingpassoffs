d93da4269d1bfd18692fdb9806a1219ed36bc590
ShayJStewart <silverwingshay@gmail.com>
2016-04-26 11:09:34 -0600

    Added files via upload

---
+++ Sword.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : Weapons 
+{
+	public Sword()
+	{
+		type = "Sword";
+		Debug.Log ("1st Sword constructor Called");
+	}
+
+	public Sword(string newType) : base(newType)
+	{
+		Debug.Log ("2nd Sword Constructor Called");
+	}
+	//in this script we are telling the program how to determine that a sword is a sword and that it is a weapon
+}
+
---
+++ Weapons.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapons 
+{
+
+	public class Weapons
+	{
+		//we are going to determine what to do with the weapon by deciding if it is a sword or a bow
+		public string type;
+
+		//first constructor for weapons class
+		public Weapons()
+		{
+			type = "sword";
+			Debug.Log("1st Weapons Constructor Called");
+		}
+		//second constructor
+		public Weapons(string newType)
+		{
+			type = newType;
+			Debug.Log("2nd Weapons Constructor Called");
+		}
+		//main things that each weapon can do act like no matter what child class they are
+		public void Swing()
+		{
+			Debug.Log("The" + type + "has been used.");
+		}
+
+		public void Funny()
+		{
+			Debug.Log("I'm real warrior now!");
+		}
+	
+	}
+}
