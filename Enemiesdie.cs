a5c8be089466369a4dbcdc63c9bc3e8969fef951
ShayJStewart <silverwingshay@gmail.com>
2016-04-26 11:55:54 -0600

    Added files via upload

---
+++ Enemiesdie.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemiesdie : MonoBehaviour
+{
+	int CurrentEnemies = 3;
+
+	// code for enemy kill count using a loop
+	void Start () {
+		while (CurrentEnemies > 0)
+		{
+			Debug.Log ("I've killed an enemy, Huzzah!");
+			CurrentEnemies--;
+		}
+		Debug.Log ("Yay! I've killed all of them!");
+	
+	}
+}
